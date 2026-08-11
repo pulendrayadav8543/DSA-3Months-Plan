549fff300e7d805dcfc4236ffaa1bf6ce08279b6
pulendrayadav8543 <pulendrakumar.scsebtech@gmail.com>
2026-08-11 23:24:11 +0530

    Hi.java file deleted

--- Hi.java
+++
@@ -1,8 +0,0 @@
-public class Hi{
-  public static int add(int a,int b){
-	System.out.println("Sum in Class Hi");
-  }
-  public static void main(String[] args){
-    System.out.println("Hii in Java Class Hi");
-  }
-}
