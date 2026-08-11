24145156004c7d24d599834dbeb48ec4905dce6a
pulendrayadav8543 <pulendrakumar.scsebtech@gmail.com>
2026-08-11 23:20:36 +0530

    sum method add in Hi Class

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
+//https://github.com/pulendrayadav8543/DSA-3Months-Plan.git
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
--- Hi.java
+++ Hi.java
@@ -1,4 +1,7 @@
 public class Hi{
+  public static int add(int a,int b){
+	System.out.println("Sum in Class Hi");
+  }
   public static void main(String[] args){
     System.out.println("Hii in Java Class Hi");
   }
