244d1c9823c76699ffcb6785a8df9cdb51b71e7b
PulendraLocalRepo <pulendralocalrepo@gmail.com>
2026-08-13 08:22:39 +0530

    P.java file and Hello.java file deleted

--- Hello.java
+++
@@ -1,7 +0,0 @@
-//https://github.com/pulendrayadav8543/DSA-3Months-Plan.git
-public class Hello{
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.println("Hello i am lazy");
-	}
-}
--- P.java
+++
@@ -1,8 +0,0 @@
-public class P{
-	int a;
-	String str;
-	void m1(){
-		System.out.println("M1 in side P class");
-		System.out.println("one new method add in class P");
-	}
-}
